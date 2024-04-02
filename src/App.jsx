953ddd01848453d24a0104f4ec413d75018ae5f0
zen <zenon.deviagge@gmail.com>
2024-04-02 20:23:36 -0300

    Setup From component. Verify is basic functionality work by testing if input is display in the console by clicking Add Item button.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import Form from "./Form";
+
+const App = () => {
+  const [items, setItems] = useState([])
+  return <section className="section-center">
+    <Form/>
+  </section>;
+};
+
+export default App;
