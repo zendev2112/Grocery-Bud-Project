1f48b3308d3ff90704824d6e96def4204b763867
zen <zenon.deviagge@gmail.com>
2024-04-03 12:58:31 -0300

    Style text of completed SingleItem component.

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,8 +1,22 @@
-const SingleItem = ( {item, removeItem} ) => {
+import { useState } from 'react'
+
+const SingleItem = ({ item, removeItem }) => {
+  const [isChecked, setIsChecked] = useState(item.completed)
   return (
-    <div className='single-item'>
-      <input type="checkbox" />
-      <p>{item.name}</p>
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+      />
+      <p
+        style={{
+          textTransform: 'capitalize',
+          textDecoration: isChecked && 'line-through',
+        }}
+      >
+        {item.name}
+      </p>
       <button className="btn remove-btn" type="button">
         delete
       </button>
