44b65bd4ff3b6dad7e151bb3cf09830a3866531f
zen <zenon.deviagge@gmail.com>
2024-04-03 16:35:29 -0300

    Add toasts for empty input, item added and item deleted.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { nanoid } from 'nanoid'
 import Form from './Form'
 import Items from './items'
+import { ToastContainer, toast } from 'react-toastify'
 
 const getLocalStorage = () => {
   let list = localStorage.getItem('list')
@@ -29,13 +30,15 @@ const App = () => {
     }
     const newItems = [...items, newItem]
     setItems(newItems)
-    setLocalStorage(newItems)
+    setLocalStorage(newItems);
+    toast.success('item added to the list')
   }
 
   const removeItem = (itemId) => {
     const newItems = items.filter((item) => item.id !== itemId)
     setItems(newItems)
-    setLocalStorage(newItems)
+    setLocalStorage(newItems);
+    toast.success('item deleted')
   }
 
   const editItem = (itemId) => {
@@ -52,6 +55,7 @@ const App = () => {
 
   return (
     <section className="section-center">
+      <ToastContainer position='top-center'/>
       <Form addItem={addItem} />
       <Items items={items} removeItem={removeItem} editItem={editItem}/>
     </section>
