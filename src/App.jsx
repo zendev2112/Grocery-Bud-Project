3a516c709835d2bfbc4bf8d5cb797496eb3a6ec4
zen <zenon.deviagge@gmail.com>
2024-04-02 22:45:46 -0300

    Create Items component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Form from "./Form";
 import { nanoid } from "nanoid";
+import Items from "./items";
 const App = () => {
   const [items, setItems] = useState([]);
 
@@ -11,9 +12,12 @@ const App = () => {
       name:itemName,
     };
     setItems([...items, newItem])
-  }
+  };
+
+  const removeItem = (itemId) => {}
   return <section className="section-center">
     <Form addItem={addItem}/>
+    <Items addItem={addItem} removeItem={removeItem}/>
   </section>;
 };
 
---
+++ src/items.jsx
@@ -0,0 +1,6 @@
+const Items = () => {
+  return (
+    <h2>items</h2>
+  )
+}
+export default Items
